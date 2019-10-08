5135628cd35113c7095d8107093d2562a69ec7f1
Max Karlas <327675@student.mboutrecht.nl>
2019-10-08 09:58:24 +0200

    apd website met 6 plaatjes (buttons werken)

---
+++ js/app.js
@@ -0,0 +1,48 @@
+function showwinner() {
+    let p_tag;
+    p_tag = document.querySelector("#p_01");
+    p_tag.innerHTML = "Ferguson heeft deze wedstrijd gewonnen";
+    let img_tag;
+    img_tag = document.querySelector("#img_01");
+    img_tag.setAttribute("src", "./img/ferguson.jpg");
+}
+function showwinner2() {
+    let p_tag;
+    p_tag = document.querySelector("#p_02");
+    p_tag.innerHTML = "Khabib heeft deze wedstrijd gewonnen";
+    let img_tag;
+    img_tag = document.querySelector("#img_02");
+    img_tag.setAttribute("src", "./img/khabib.jpg");
+}
+function showwinner3() {
+    let p_tag;
+    p_tag = document.querySelector("#p_03");
+    p_tag.innerHTML = "Lewis heeft deze wedstrijd gewonnen";
+    let img_tag;
+    img_tag = document.querySelector("#img_03");
+    img_tag.setAttribute("src", "./img/lewis.jpg");
+}
+function showwinner4() {
+    let p_tag;
+    p_tag = document.querySelector("#p_04");
+    p_tag.innerHTML = "Ferguson heeft deze wedstrijd gewonnen";
+    let img_tag;
+    img_tag = document.querySelector("#img_04");
+    img_tag.setAttribute("src", "./img/ferguson.jpg");
+}
+function showwinner5() {
+    let p_tag;
+    p_tag = document.querySelector("#p_05");
+    p_tag.innerHTML = "Khabib heeft deze wedstrijd gewonnen";
+    let img_tag;
+    img_tag = document.querySelector("#img_05");
+    img_tag.setAttribute("src", "./img/khabib.jpg");
+}
+function showwinner6() {
+    let p_tag;
+    p_tag = document.querySelector("#p_06");
+    p_tag.innerHTML = "Lewis heeft deze wedstrijd gewonnen";
+    let img_tag;
+    img_tag = document.querySelector("#img_06");
+    img_tag.setAttribute("src", "./img/lewis.jpg");
+}
---
+++ ts/app.ts
@@ -0,0 +1,83 @@
+function showwinner(): void {
+    let p_tag: HTMLParagraphElement;
+
+    p_tag = document.querySelector("#p_01");
+
+    p_tag.innerHTML = "Ferguson heeft deze wedstrijd gewonnen";
+
+    let img_tag: HTMLImageElement;
+
+    img_tag = document.querySelector("#img_01");
+
+    img_tag.setAttribute("src","./img/ferguson.jpg");
+}
+
+function showwinner2(): void {
+    let p_tag: HTMLParagraphElement;
+
+    p_tag = document.querySelector("#p_02");
+
+    p_tag.innerHTML = "Khabib heeft deze wedstrijd gewonnen";
+
+    let img_tag: HTMLImageElement;
+
+    img_tag = document.querySelector("#img_02");
+
+    img_tag.setAttribute("src","./img/khabib.jpg");
+}
+
+function showwinner3(): void {
+    let p_tag: HTMLParagraphElement;
+
+    p_tag = document.querySelector("#p_03");
+
+    p_tag.innerHTML = "Lewis heeft deze wedstrijd gewonnen";
+
+    let img_tag: HTMLImageElement;
+
+    img_tag = document.querySelector("#img_03");
+
+    img_tag.setAttribute("src","./img/lewis.jpg");
+}
+
+function showwinner4(): void {
+    let p_tag: HTMLParagraphElement;
+
+    p_tag = document.querySelector("#p_04");
+
+    p_tag.innerHTML = "Ferguson heeft deze wedstrijd gewonnen";
+
+    let img_tag: HTMLImageElement;
+
+    img_tag = document.querySelector("#img_04");
+
+    img_tag.setAttribute("src","./img/ferguson.jpg");
+}
+
+function showwinner5(): void {
+    let p_tag: HTMLParagraphElement;
+
+    p_tag = document.querySelector("#p_05");
+
+    p_tag.innerHTML = "Khabib heeft deze wedstrijd gewonnen";
+
+    let img_tag: HTMLImageElement;
+
+    img_tag = document.querySelector("#img_05");
+
+    img_tag.setAttribute("src","./img/khabib.jpg");
+}
+
+function showwinner6(): void {
+    let p_tag: HTMLParagraphElement;
+
+    p_tag = document.querySelector("#p_06");
+
+    p_tag.innerHTML = "Lewis heeft deze wedstrijd gewonnen";
+
+    let img_tag: HTMLImageElement;
+
+    img_tag = document.querySelector("#img_06");
+
+    img_tag.setAttribute("src","./img/lewis.jpg");
+}
